ba3d2b86248d7a897c6ff848ace31e408bd1adc2
halcyon-past <titanssuperior@gmail.com>
2026-05-03 17:22:46 +0530

    added better handling of uvicorn

--- backend/main.py
+++ backend/main.py
@@ -253,3 +253,8 @@ async def on_shutdown():
 	if coros:
 		await asyncio.gather(*coros)
 	pcs.clear()
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
